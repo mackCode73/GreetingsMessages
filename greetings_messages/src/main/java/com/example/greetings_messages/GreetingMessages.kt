02d21e5e4b36ee9224b36df0ab9cee6e2a4d8061
manoj kumar <manoj.kumar@jarvis.consulting>
2022-04-14 11:40:20 +0530

    base commit

--- greetings_messages/src/main/java/com/example/greetings_messages/GreetingMessages.kt
+++ greetings_messages/src/main/java/com/example/greetings_messages/GreetingMessages.kt
@@ -2,14 +2,32 @@ package com.example.greetings_messages
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.AttributeSet
-import android.widget.TextView
-import com.bumptech.glide.Glide
-import de.hdodenhof.circleimageview.CircleImageView
+import android.util.Log
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.updateLayoutParams
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import de.hdodenhof.circleimageview.BuildConfig.APPLICATION_ID
+import de.hdodenhof.circleimageview.CircleImageView
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 
 
 class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyleAttr: Int = 0) :
@@ -25,11 +43,15 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
 
 
     var greetingHeadingTextView: TextView? = null
-    var greetingHeadingImageView: CircleImageView? = null
+    var greetingHeadingImageView: ImageView? = null
     val Int.px get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+    // var imageBitmap: Bitmap? = null
+    var imageUri : Uri? = null
 
 
     fun setImageDrawbleAndHeading(imageUrl: String, greetingMsg: String) {
+
+        var imageBitmap: Bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
 
         // set-up parent constraint layout
           this.updateLayoutParams<LayoutParams> {
@@ -46,69 +68,98 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
                     //add other constraints if needed
                 }
 
-        greetingHeadingImageView = CircleImageView(context)
+        greetingHeadingImageView = ImageView(context)
         greetingHeadingImageView!!.id = R.id.item1
         greetingHeadingImageView!!.apply {
             this.layoutParams = ViewGroup.LayoutParams(
-                150.px,
-                150.px
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
             )
-            this.maxWidth = 100
-            this.maxHeight = 100
+            this.maxWidth = 400
+            this.maxHeight = 800
             this.setPadding(50, 50, 50, 50)
         }
 
 
             if (imageUrl.isNotEmpty()) {
-                Glide
+               /* Glide
                     .with(context)
                     .load(imageUrl)
-                    .into(greetingHeadingImageView!!)
+                    .into(greetingHeadingImageView!!)*/
 
-                /*  Glide
-                      .with(context)
-                      .asBitmap()
-                      .load(imageUrl)
-                      .into(object : CustomTarget<Bitmap>(){
-                          override fun onResourceReady(
-                              resource: Bitmap,
-                              transition: Transition<in Bitmap>?
-                          ) {
-                              imageBitmap = resource
-                          }
+                 try {
+                     Glide
+                         .with(context)
+                         .asBitmap()
+                         .load(imageUrl)
+                         .into(object : CustomTarget<Bitmap>(){
+                             override fun onResourceReady(
+                                 resource: Bitmap,
+                                 transition: Transition<in Bitmap>?
+                             ) {
+                                 imageBitmap = resource
+                                 imageUri = getUriFromBitmap(imageBitmap , greetingMsg , context)
+                                 greetingHeadingImageView!!.setImageURI(imageUri)
+                             }
 
-                          override fun onLoadCleared(placeholder: Drawable?) {
-                              TODO("Not yet implemented")
-                          }
+                             override fun onLoadCleared(placeholder: Drawable?) {
+                                 TODO("Not yet implemented")
+                             }
 
-                      })*/
+                         })
+                 }catch (exception : Exception){ exception.printStackTrace() }
             }
+
+            //getUriFromBitmap
+
+
 
             //    imageBitmapTemp = imageBitmap?.let { it1 -> writeOnBitmap(it1 , greetingMsg) }
             //   greetingsBgViewBinding[index].setImageBitmap(imageBitmap)
-            addView(greetingHeadingImageView)
+             addView(greetingHeadingImageView)
 
             //setup textview constraints
-            val constraintLayout  = ConstraintLayout.LayoutParams(LayoutParams.MATCH_PARENT , LayoutParams.MATCH_PARENT)
+            /*val constraintLayout  = ConstraintLayout.LayoutParams(LayoutParams.MATCH_PARENT , LayoutParams.MATCH_PARENT)
             constraintLayout.startToEnd = greetingHeadingImageView!!.id
             constraintLayout.endToEnd = ConstraintSet.PARENT_ID
             constraintLayout.topToTop = ConstraintSet.PARENT_ID
-            constraintLayout.topMargin = 50
+            constraintLayout.topMargin = 50*/
 
 
 
             //     adding TextView
-            greetingHeadingTextView = TextView(context)
+          /*  greetingHeadingTextView = TextView(context)
             greetingHeadingTextView?.apply {
                 greetingHeadingTextView?.setPadding(10, 10, 10, 10)
                 greetingHeadingTextView?.text = greetingMsg
-                greetingHeadingTextView!!.layoutParams = constraintLayout
+                greetingHeadingTextView!!.layoutParams = constraintLayout*/
             }
-         //   removeAllViews()
-            addView(greetingHeadingTextView)
+
+    private fun getUriFromBitmap(imageBitmap: Bitmap?, greetingMsg: String, context: Context): Uri? {
+        val bmp = writeOnBitmap(imageBitmap as Bitmap , greetingMsg , context)
+        val imagefolder = File(context.cacheDir, "images")
+        var uri: Uri? = null
+        try {
+            Log.v(GreetingMessages::class.qualifiedName, "getUriFromBitmap() try block...")
+            imagefolder.mkdirs()
+            val file = File(imagefolder, "shared_image.png")
+            val outputStream = FileOutputStream(file)
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, outputStream)
+            outputStream.flush()
+            outputStream.close()
+         //   uri = FileProvider.getUriForFile(context,"com.example.greetingsmessages", file)
+           uri =  FileProvider.getUriForFile(Objects.requireNonNull(context), "com.example.greetingsmessages" + ".provider", file)
+        } catch (e: Exception) {
+            Log.v(GreetingMessages::class.qualifiedName, "getUriFromBitmap() $e")
+            Toast.makeText(context, "${e.message}", Toast.LENGTH_LONG).show()
+        }
+        Log.v(GreetingMessages::class.qualifiedName, "getUriFromBitmap() return uri is $uri")
+        return uri
+    }
+    //   removeAllViews()
+   //            addView(greetingHeadingTextView)
 
         }
-    }
 
     // extension function to get bitmap from url
     /* private fun imageUrltoBitmap(imageUrl: String): Bitmap? {
@@ -131,7 +182,8 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
          }
      }*/
 
-    /*private fun writeOnBitmap(bitmap: Bitmap, greetingMsg: String): Bitmap {
+    private fun writeOnBitmap(bitmap: Bitmap, greetingMsg: String, context: Context): Bitmap {
+        Log.v(GreetingMessages::class.qualifiedName, "writeOnBitmap()")
         val smallProfileImage = Bitmap.createScaledBitmap(
             bitmap,
             300,
@@ -146,9 +198,9 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
             )
         )
         val resultImage =
-            Bitmap.createBitmap(bitmap.width + 200, bitmap.height + 200, bitmap.config)
+            Bitmap.createScaledBitmap(bitmap, 700,1000 , false)
         val canvas = Canvas(resultImage)
-        canvas.drawBitmap(bitmap, 0f, 0f, null)
+     ///   canvas.drawBitmap(bitmap, 0f, 0f, null)
 
         val paint = Paint()
         paint.isAntiAlias = (true)
@@ -156,9 +208,9 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
         paint.textSize = (30f)
         canvas.drawRect(
             0f,
-            bitmap.height.toFloat(),
-            bitmap.width.toFloat(),
-            bitmap.height + 100.toFloat(),
+            900f,
+            700f,
+            1000f,
             paint
         )
 
@@ -168,19 +220,19 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
                 R.color.design_default_color_secondary_variant
             )
         )
-        canvas.drawText(greetingMsg, 100f, bitmap.height.toFloat() + 40, paint)
+        canvas.drawText(greetingMsg, 100f, 900f + 40, paint)
         canvas.drawText(
             "Contact us : ${greetingMsg}",
             100f,
-            bitmap.height.toFloat() + 40 + 40,
+            900f + 40 + 40,
             paint
         )
         paint.color = ContextCompat.getColor(context, R.color.design_default_color_primary_dark)
-        canvas.drawCircle(45f, bitmap.height.toFloat() + 50f, 30f, paint)
+        canvas.drawCircle(45f, 900f + 50f, 30f, paint)
         val path = Path()
         path.apply {
             addCircle(
-                45f, bitmap.height.toFloat() + 50f,
+                45f, 900f + 50f,
                 25f,
                 Path.Direction.CCW
             )
@@ -190,9 +242,10 @@ class GreetingMessages(context: Context, attrsSet: AttributeSet? = null, defStyl
         canvas.clipPath(path)
         canvas.drawBitmap(
             smallProfileImage, 20f,
-            bitmap.height.toFloat() + 25f, null
+            900f + 25f, null
         )
 
+        Log.v(GreetingMessages::class.qualifiedName, "writeOnBitmap() result image is ${resultImage}")
         return resultImage
 
-    }*/
+    }
